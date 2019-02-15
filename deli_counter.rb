5a003212f3f0e677b302bf5e371d21119f857ab3
Trey <tbeau5595@hotmail.com>
2019-02-15 03:48:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ end
 
 def take_a_number(number, name)
   number << name
-  puts "Welcome, #{name}. You are number #{number} "
+  puts "Welcome, #{name}. You are number #{number.length} in line."
 end
 
 def now_serving
